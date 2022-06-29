4eb0216e9b62eb02a12b80b4c691a393ec4c43b8
BEN <pwc123451@gmail.com>
2022-06-28 20:10:36 -0400

    begin

---
+++ chapter1/textbookSolutions.c
@@ -0,0 +1,21 @@
+
+/* main.c */
+/* 0. copyright/licensing */
+/* 1. includes */
+/* 2. defines */
+/* 3. external declarations */
+/* 4. typedefs */
+/* 5. global variable declarations */
+/* 6. function prototypes */
+#include <stdio.h>
+int main (int argc, char *argv[]) {
+    //0 means false
+    if (0) {
+        printf("dancing\n");
+    }
+    //every other value is true
+    if (1) printf("yooooo\n");
+    if (32) printf("it's fine\n");
+
+    return 0;
+}
